894b8057d855c892a4a022e3f67edb2c42508a3a
MIKEY <109261767+mikeyoni@users.noreply.github.com>
2026-03-20 00:02:07 +0600

    login and resigter setup
    done

--- CHAT_INIT/main.go
+++ CHAT_INIT/main.go
@@ -1,11 +1,44 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/charmbracelet/lipgloss"
 )
+
+// structure of the json data base
+
+type user struct {
+	UserName string `json:"username"`
+	Email    string `json:"email"`
+	Token    string `json:"token"`
+	Password string `json:"passowrd"`
+}
+
+// json file read
+
+func jsonreade() ([]user, error) {
+
+	var jsondata []user
+	bytes, err := os.ReadFile("database.json")
+	if err != nil {
+
+		return nil, err
+
+	}
+
+	err = json.Unmarshal(bytes, &jsondata)
+	if err != nil {
+
+		return nil, err
+	}
+
+	return jsondata, nil
+
+}
 
 // the login funciton
 
@@ -16,20 +49,75 @@ func login(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf(" login page faild ! : %s ", http.StatusNotFound)
 		return
 	}
-	if r.Method != "GET" {
-		fmt.Printf(" login page faild ! methode ! : %s ", http.StatusNotFound)
-		return
-	}
 
 	fmt.Fprintf(w, "\n Login page is active \n")
 
-	// Inside your login handler on the server:
-	email := r.FormValue("email")
-	pass := r.FormValue("password")
+	var incamingdata struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+
+	json.NewDecoder(r.Body).Decode(&incamingdata)
+
+	fmt.Printf(" %+v ", incamingdata)
+
+}
+
+// this is register function
+
+func register(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/signup" {
+
+		fmt.Printf(" signup page faild : %s ", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, " \n Signup page is active <3 \n")
+
+	var incamingdats struct {
+		Email    string `json:"email"`
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+
+	json.NewDecoder(r.Body).Decode(&incamingdats)
+
+	fmt.Printf(" %+v ", incamingdats)
+
+}
+
+// this is forget password email capture
+
+func forgetpass(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/forgetpass" {
+		fmt.Printf(" faild show forget passpage : %+v", http.StatusNotFound)
+		return
+	}else{
+
+		fmt.Fprintf(w, " \n Forgetpassrod gape is active :D \n")
+	}
+
+	var imcamingdata struct {
+		Email string `json:"email"`
+	}
+
+	json.NewDecoder(r.Body).Decode(&imcamingdata)
+
+	fmt.Printf(" \n we got the email that forget passwrod : %+v ", imcamingdata)
 
 }
 
 func main() {
+
+	// jsondata, err := jsonreade()
+	// if err != nil {
+	// 	fmt.Printf(" Faild to Load json file : " , err)
+	// 	return
+	// }
+
+	// fmt.Printf("\n %+v \n", jsondata )
 
 	// this are only style of text colors in terminal
 
@@ -39,8 +127,9 @@ func main() {
 	// this are the main functin that canect logical funcion to the url quaris
 
 	http.HandleFunc("/login", login)
-	// http.HandleFunc("/register" , long)
-	// http.HandleFunc("/forgetpass" , long)
+	http.HandleFunc("/signup", register)
+	http.HandleFunc("/forgetpass", forgetpass)
+
 	// http.HandleFunc("/chat-init" , long)
 
 	// server startig indicatin
@@ -51,7 +140,7 @@ func main() {
 
 	if err := http.ListenAndServe(":4040", nil); err != nil {
 
-		fmt.Println(" local Fort 8080 is unusebal right this moment ! ", err)
+		fmt.Println(" local Fort 4040 is unusebal right this moment ! ", err)
 
 	}
 
--- CLIENT/main.go
+++ CLIENT/main.go
@@ -1,13 +1,109 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"net/http"
+	// "net/url"
 	// "net/http"
 )
 
+// this is login function it do post login info in a url in json form package
 
-func main(){
+func login(url string, username string, password string) {
 
-	fmt.Printf(" THE client is active ! \n")
+	data := map[string]string{
+
+		"username": username,
+		"password": password,
+	}
+
+	jsondata, _ := json.Marshal(data)
+
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsondata))
+
+	if err != nil {
+
+		fmt.Printf("\n Faild to sent post req : ", err)
+		return
+	} else {
+
+		fmt.Printf(" \n sucessfully sented : ", resp.Status)
+	}
+
+	defer resp.Body.Close()
+
+}
+
+// this is register function this do post register info to the srever in json form
+
+func register(url string, email string, username string, password string) {
+
+	data := map[string]string{
+
+		"email":    email,
+		"username": username,
+		"password": password,
+	}
+
+	jsondata, err := json.Marshal(data)
+	if err != nil {
+
+		fmt.Printf(" \n faild to marshel json data : ", err)
+		return
+	}
+	resp, err := http.Post(url, "application/json-data", bytes.NewBuffer(jsondata))
+
+	if err != nil {
+		fmt.Printf(" \n failt to post register info : ", err)
+		return
+	} else {
+
+		fmt.Printf(" \n successfuly posted register data : ", err)
+
+	}
+
+	defer resp.Body.Close()
+
+}
+
+// forget password
+
+func forgetpass(url string, email string) {
+
+	data := map[string]string{
+
+		"email": email,
+	}
+
+	jsondata, err := json.Marshal(data)
+
+	if err != nil {
+
+		fmt.Printf(" \n faild to marshal json data : %+s ", err)
+		return
+	}
+
+	resp, err := http.Post(url, "/application/paasforget", bytes.NewBuffer(jsondata))
+
+	if err != nil {
+
+		fmt.Printf(" \n failt to sent the email to server : %+s ", err)
+		return
+	} else {
+		fmt.Printf(" \n success fully sented forget password email : %+s", resp.Status)
+
+	}
+
+	defer resp.Body.Close()
+
+}
+
+func main() {
+
+	login("http://localhost:4040/login", "mikey", "hahah ")
+	register("http://localhost:4040/signup", "mda2343@gmail.com", "draken", "paf32453")
+	forgetpass("http://localhost:4040/forgetpass", "mda35345345@gmail.com")
 
 }
